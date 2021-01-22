52a8f6425a8dc42530359383b3bc14d2fec21581
Utkarshp1 <mikuprakashmiku@yahoo.com>
2021-01-22 20:51:25 +0530

    Added comments

--- Lab2/main.py
+++ Lab2/main.py
@@ -2,7 +2,6 @@ import sys
 import time
 from Node import Node
 from utils import read_input
-from utils import move_gen
 from bfs import best_first_search
 from heuristic import heuristic1
 from heuristic import heuristic2
@@ -15,24 +14,23 @@ if len(sys.argv) != 4:
     print("Heuristic Choice:\n\tA number between 1 and 3 (both inclusive)")
     quit()
 
-file = open(sys.argv[1], "r")       # Reading test case file
+file = open(sys.argv[1], "r")       # Reading test case file name
 file.readline()                     
 
-start_node = Node()
-read_input(start_node, file)
+start_node = Node()                 # Creating start node object
+read_input(start_node, file)        # reading the initial state    
 
+# ignoring the extra lines
 file.readline()
 file.readline()
 
-goal_node = Node()
-read_input(goal_node, file)
-
-print(start_node.blocks)
-print(goal_node.blocks)
+goal_node = Node()                  # Creating goal node object
+read_input(goal_node, file)         # reading the goal state
 
 algo_choice = sys.argv[2]
 heuristic_choice = int(sys.argv[3])
 
+# Calculating the heuristic value for start node and goal node
 heuristic = None
 if heuristic_choice == 1:
     heuristic = heuristic1
@@ -49,17 +47,16 @@ elif heuristic_choice == 3:
 else:
     print("The heuristic choice should be between 1 and 3 (both inclusive)")
 
+# Using the appropriate algorithm
 if algo_choice == "bfs":
     start_time = time.time()
-    print(best_first_search(start_node, goal_node, heuristic))
+    result = best_first_search(start_node, goal_node, heuristic)
+    print("States Explored: {}\nPath Length: {}\n".format(result[0], result[1]))
     print("Time taken: " + str(time.time() - start_time))
 elif algo_choice == "hill":
     start_time = time.time()
-    print(hill_climb(start_node, goal_node, heuristic))
+    result = hill_climb(start_node, goal_node, heuristic)
+    print("States Explored: {}\nPath Length: {}\n".format(result[0], result[1]))
     print("Time taken: " + str(time.time() - start_time))
 else:
     print("The algorithm chosen should be either `bfs` or `hill` without quotes")
-
-# print(heuristic1(goal_node, goal_node))
-# print(heuristic2(goal_node, goal_node))
-# print(heuristic3(goal_node, start_node))
